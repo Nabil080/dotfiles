4b7b9b6554d49ae1a02c8afd9a343147f0780fa2
Nabil080 <bellilanabil8@gmail.com>
2025-06-20 21:43:09 +0200

    nvim simple keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,2 +1,40 @@
 -- Keymaps for core/editor stuff only (like save, quit, window movement).
 -- Do NOT add plugin-specific keymaps here — those belong in the corresponding plugin spec file (in lua/plugins).
+-- keymap.set({mode}, {key}, {action}, {opts})
+
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- arrow keys
+keymap.set("n", "<Left>", '<cmd>echo "Stop using arrow keys"<CR>')
+keymap.set("n", "<Right>", '<cmd>echo "Stop using arrow keys"<CR>')
+keymap.set("n", "<Up>", '<cmd>echo "Stop using arrow keys"<CR>')
+keymap.set("n", "<Down>", '<cmd>echo "Stop using arrow keys"<CR>')
+
+-- x won't copy to register
+keymap.set({'n', 'x'}, 'x', '"_x')
+keymap.set({'n', 'x'}, 'X', '"_d')
+
+-- select all text
+keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
